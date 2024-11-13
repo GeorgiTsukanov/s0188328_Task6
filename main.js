7c09915fc9e4f6fc7c667e4f5bc89a848fba4d71
GeorgiTsukanov <georgicukanov@gmail.com>
2024-11-13 04:13:37 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,126 @@
+function updatePrice() {
+    // Находим select по имени в DOM.
+    let s = document.getElementsByName("prodType");
+    let select = s[0];
+    let price = 0;
+    let prices = getPrices();
+    let priceIndex = parseInt(select.value) - 1;
+    if (priceIndex >= 0) {
+      price = prices.prodTypes[priceIndex];
+    }
+    
+    let radioDiv = document.getElementById("radios");
+    let checkDiv = document.getElementById("checkboxes");
+
+    if (select.value == "1") {
+        // Для первого элемента скрываем чекбоксы и показываем радиокнопки.
+        checkDiv.style.display = "none";
+        radioDiv.style.display = "none";
+        resetRadios();
+        resetCheckboxes();
+    } else if (select.value == "2") {
+        // Для второго элемента показываем радиокнопки, скрываем чекбоксы.
+        checkDiv.style.display = "none";
+        radioDiv.style.display = "block";
+        resetCheckboxes();
+    } else {
+        // Для третьего элемента скрываем радиокнопки и показываем чекбоксы.
+        checkDiv.style.display = "block";
+        radioDiv.style.display = "none";
+        resetRadios();
+    }
+
+    // Смотрим какая товарная опция выбрана.
+    let radios = document.getElementsByName("prodOptions");
+    radios.forEach(function(radio) {
+      if (radio.checked) {
+        let optionPrice = prices.prodOptions[radio.value];
+        if (optionPrice !== undefined) {
+          price += optionPrice;
+        }
+      }
+    });
+    
+    // Смотрим какие товарные свойства выбраны.
+    let checkboxes = document.querySelectorAll("#checkboxes input");
+    checkboxes.forEach(function(checkbox) {
+      if (checkbox.checked) {
+        let propPrice = prices.prodProperties[checkbox.name];
+        if (propPrice !== undefined) {
+          price += propPrice;
+        }
+      }
+    });
+
+    let prodPrice = document.getElementById("res");
+    let amount = document.getElementById('amount');
+    if (amount.value.match(/^[1-9]\d*$/) === null) {
+        prodPrice.innerHTML = "введены неверные значения количества";
+    }
+    else{
+        prodPrice.innerHTML = price*amount.value + " рублей";
+    }
+  }
+  
+
+  function resetRadios() {
+    let radios = document.getElementsByName("prodOptions");
+    radios.forEach(function(radio) {
+        radio.checked = false;
+    });
+}
+
+function resetCheckboxes() {
+    let checkboxes = document.querySelectorAll("#checkboxes input");
+    checkboxes.forEach(function(checkbox) {
+        checkbox.checked = false;
+    });
+}
+
+  function getPrices() {
+    return {
+      prodTypes: [150, 10000, 12000],
+      prodOptions: {
+        option1: 0,
+        option2: 1000,
+        option3: 1700,
+      },
+      prodProperties: {
+        prop1: 1000,
+        prop2: 750,
+      }
+    };
+  }
+  
+  window.addEventListener('DOMContentLoaded', function (event) {
+    let s = document.getElementsByName("prodType");
+    let select = s[0];
+    select.addEventListener("change", function(event) {
+      updatePrice();
+    });
+      
+    let radios = document.getElementsByName("prodOptions");
+    radios.forEach(function(radio) {
+      radio.addEventListener("change", function(event) {
+        updatePrice();
+      });
+    });
+
+    // Обработчик изменения количества
+    let inputs = document.querySelectorAll("input");
+    inputs.forEach(function(input) {
+        input.addEventListener("change", function(event) {
+            updatePrice();
+        });
+    });
+  
+    let checkboxes = document.querySelectorAll("#checkboxes input");
+    checkboxes.forEach(function(checkbox) {
+      checkbox.addEventListener("change", function(event) {
+        updatePrice();
+      });
+    });
+  
+    updatePrice();
+  });
+  
